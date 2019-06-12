f18b3d59e7253c6abcee1865ad99aefc4ba7c37e
98MM <47939788+98MM@users.noreply.github.com>
2019-06-12 18:33:26 +0200

    Update Front_Back.js

--- Scripts/Front_Back.js
+++ Scripts/Front_Back.js
@@ -1,23 +1,26 @@
 // -----JS CODE-----
+//This script enables/disables elements based on which side of the camera is active.
+//Simply add it as an object, set it to initialized and add your own components and you're set.
 //@input Component.YouWant toHide
 
 if(global.scene.getCameraType() == "front")
 {
-    script.toHide.enabled = true;		//if you want it on front, set true, else false
+    script.toHide.enabled = true;		//script.toHide will be active on the front camera side
 }
 else if(global.scene.getCameraType() == "back")
 {
-    //false
+    //(false) You define what goes on the back side here
 }
+
 
 var event = script.createEvent("CameraFrontEvent");
 event.bind(function (eventData)
 {
-    //true
+    //see above for "front"
 });
 
 var event = script.createEvent("CameraBackEvent");
 event.bind(function (eventData)
 {
-    //false
+    //see above for "back"
 });
